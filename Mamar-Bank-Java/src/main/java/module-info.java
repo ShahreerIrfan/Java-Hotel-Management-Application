4e776a4739e6f0679296c8e5dd67189218b8f4f4
Your Name <mdshahreerirfan@gmail.com>
2024-01-16 19:09:45 +0600

    project setup done

--- Mamar-Bank-Java/src/main/java/module-info.java
+++ Mamar-Bank-Java/src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module com.mamar.bank.mamarbankjava {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.mamar.bank.mamarbankjava to javafx.fxml;
     exports com.mamar.bank.mamarbankjava;
+    exports  com.mamar.bank.mamarbankjava.Controllers;
+    exports com.mamar.bank.mamarbankjava.Controllers.Admin;
+    exports com.mamar.bank.mamarbankjava.Controllers.Client;
+    exports com.mamar.bank.mamarbankjava.Models;
+    exports com.mamar.bank.mamarbankjava.Views;
 }
